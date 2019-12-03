1e705fac21e6192fbc6225774af8ceec8480b483
unknown <marijetabl@gmail.com>
2019-12-03 22:32:26 +0100

    Added Signin and Signup form

--- hci-project/src/pages/public_pages/signin.js
+++ hci-project/src/pages/public_pages/signin.js
@@ -2,15 +2,25 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../../components/layout"
+import Style from "../../styles/signup-in.css"
 
 const SignIn = () => (
   <Layout>
     <h1>Sign in</h1>
 
-    <ul>
-        <li><Link to="/private_pages/cart">Cart</Link></li>
-        <li><Link to="/private_pages/bookmarks">Bookmarks</Link></li>
-    </ul>
+   
+
+    <div className={Style.container}>
+      
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required></input>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required></input>
+      
+      <button type="submit">Sign in</button>
+    
+   </div>
 
     <Link to="/">Back to home</Link>
   </Layout>
--- hci-project/src/pages/public_pages/signup.js
+++ hci-project/src/pages/public_pages/signup.js
@@ -1,17 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../../components/layout"
 
+import Layout from "../../components/layout"
+import Style from "../../styles/signup-in.css"
 const SignUp = () => (
   <Layout>
     <h1>Sign up</h1>
+    
+    <div className={Style.container}>
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required></input>
 
-    <ul>
-        <li><Link to="/private_pages/cart">Cart</Link></li>
-        <li><Link to="/private_pages/bookmarks">Bookmarks</Link></li>
-    </ul>
+      <label for="email"><b>E-mail</b></label>
+      <input type="email" placeholder="Enter E-mail" name="email" required></input>
 
+      <label for="phone"><b>Phone number</b></label>
+      <input type="tel" placeholder="Enter Phone number" name="number" required></input>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required></input>
+
+      <label for="psw"><b>Repeat password</b></label>
+      <input type="password" placeholder="Repeat Password" name="psw" required></input>
+      
+      <button type="submit">Sign up</button>
+    
+   </div>
     <Link to="/">Back to home</Link>
   </Layout>
 )
