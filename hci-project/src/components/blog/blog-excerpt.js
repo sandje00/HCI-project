4d66f4af2a71021e201859ef2e6f46da6c093525
sandje00 <stella.andjelic.00@fesb.hr>
2020-02-08 17:22:37 +0100

    Responsive blog excerpt

--- hci-project/src/components/blog/blog-excerpt.js
+++ hci-project/src/components/blog/blog-excerpt.js
@@ -1,23 +1,74 @@
-import React from "react"
-import { Link } from "gatsby"
+/** @jsx jsx */
+
+import { jsx, Box } from "theme-ui"
+import { Flex } from "@theme-ui/components"
+import { navigate } from "@reach/router"
+import { useWindowWidth } from "@react-hook/window-size"
 import Image from "gatsby-image"
 
 export default ({ posts }) => {
   const postsList = posts.map(({ post }) => {
     const { id, excerpt } = post
-    const { title, date, slug, image } = post.frontmatter
+    const { title, slug, image } = post.frontmatter
+
+    const handleMoreClick = () => {
+      navigate(`public_pages/blog/${slug}`)
+    }
+
+    let width = useWindowWidth()
+    let condition = width < 826
+
     return (
-      <section key={id} >
-        <Image fixed={image.childImageSharp.fixed}/>
-        <h4>{title}</h4>
-        <span>{date}</span>
-        <p>{excerpt}</p>
-        <button>
-          <Link to={`public_pages/blog/${slug}`}>
+      <Flex 
+        key={id}
+        sx={{
+          flexDirection: condition ? "column" : "row",
+          justifyContent: "space-around",
+          alignItems: condition ? "stretch" : "center"
+        }}
+      >
+        {condition &&
+          <Box>
+            <h3>{title}</h3>
+          </Box>
+        }
+        <Box>
+          <Image 
+            fixed={image.childImageSharp.fixed}
+            sx={{ mr: "10px" }}
+          />
+        </Box>
+        <Box sx={{
+          flexShrink: "100"
+        }}>
+          {!condition &&
+            <h4 sx={{ mb: "10px" }}>
+              {title}
+            </h4>
+          }
+          <p sx={{ 
+            maxWidth: condition ? "255px" : "500px",
+            textAlign: "justify"
+          }}>
+            {excerpt}
+          </p>
+        </Box>
+        <Box>
+          <button
+            onClick={handleMoreClick}
+            sx={{
+              backgroundColor: "accent",
+              borderRadius: "10px",
+              borderStyle: "none",
+              height: "40px",
+              width: "100px",
+              textAlign: "center"
+            }}
+          >
             More
-          </Link>
-        </button>
-      </section>
+          </button>
+        </Box>
+      </Flex>
     )
   })
 
