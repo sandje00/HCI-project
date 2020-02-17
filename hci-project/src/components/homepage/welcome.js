d4334879832304fb0297d2a5876c94f63b51d578
sandje00 <stella.andjelic.00@fesb.hr>
2020-02-17 19:35:11 +0100

    Different redirection from homepage

--- hci-project/src/components/homepage/welcome.js
+++ hci-project/src/components/homepage/welcome.js
@@ -7,7 +7,7 @@ import Image from "gatsby-image"
 
 const Welcome = ({ picture }) => {
     const getStarted = () => {
-        navigate("../../tutorials")
+        navigate("../../signup")
     }
 
     return (
